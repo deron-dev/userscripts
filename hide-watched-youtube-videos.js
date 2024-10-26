4bdd945d78e9534d16135b1ec4f3542756093d7b
Deron Parker <24536673+deron-dev@users.noreply.github.com>
2024-10-26 11:29:35 -0500

    add @downloadUrl to hide-watched-youtube-videos.js

--- hide-watched-youtube-videos.js
+++ hide-watched-youtube-videos.js
@@ -6,6 +6,7 @@
 // @author       deron
 // @match        https://www.youtube.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @downloadURL  https://raw.githubusercontent.com/deron-dev/userscripts/refs/heads/master/hide-watched-youtube-videos.js
 // ==/UserScript==
 
 /** @type number */
